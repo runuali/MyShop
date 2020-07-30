0bb991fbca8d770b2ccf935fe7c74cbbc7fab1cf
Runu Ali <runuali@gmail.com>
2020-07-30 18:52:28 +0100

    Unit Testing

---
+++ MyShop.Core/MyShop.WebUI.Tests/Controllers/BasketControllerTests.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.WebUI.Tests.Mocks;
+using MyShop.Core.Models;
+using MyShop.Services;
+using System.Web.UI;
+using System.Linq;
+using MyShop.WebUI.Controllers;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web;
+using MyShop.Core.ViewModels;
+
+namespace MyShop.WebUI.Tests.Controllers
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class BasketControllerTests
+    {
+        [TestMethod]
+        public void CanAddBasketItem()
+        {
+            //Setup
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            var httpContext = new MockHttpContext();
+
+            IBasketService basketService = new BasketService(products, baskets);
+
+            //Act
+            basketService.AddToBasket(httpContext, "1");
+            Basket basket = baskets.Collection().FirstOrDefault();
+
+            //Assert
+            Assert.IsNotNull(basket);
+            Assert.AreEqual(1, basket.BasketItems.Count);
+            Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductId);
+
+
+        }
+
+        [TestMethod]
+        public void CanAddBasketItemControllerTest()
+        {
+            //Setup
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            var httpContext = new MockHttpContext();
+
+            IBasketService basketService = new BasketService(products, baskets);
+
+            var controller = new BasketController(basketService);
+            controller.ControllerContext = new ControllerContext(httpContext, new RouteData(), controller);
+
+            //Act
+            controller.AddToBasket("1");
+            Basket basket = baskets.Collection().FirstOrDefault();
+
+            //Assert
+            Assert.IsNotNull(basket);
+            Assert.AreEqual(1, basket.BasketItems.Count);
+            Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductId);
+
+        }
+
+        [TestMethod]
+        public void CanGetSummaryViewModel()
+        {
+            //Setup
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            products.Insert(new Product() { Id = "1", Price = 10.00m });
+            products.Insert(new Product() { Id = "2", Price = 5.00m });
+
+            Basket basket = new Basket();
+            basket.BasketItems.Add(new BasketItem { ProductId = "1", Quantity = 2 });
+            basket.BasketItems.Add(new BasketItem { ProductId = "2", Quantity = 1 });
+            baskets.Insert(basket);
+
+            IBasketService basketService = new BasketService(products, baskets);
+
+            var controller = new BasketController(basketService);
+            var httpContext = new MockHttpContext();
+            httpContext.Request.Cookies.Add(new HttpCookie("eCommerceBasket") { Value = basket.Id });
+            controller.ControllerContext = new ControllerContext(httpContext, new RouteData(), controller);
+
+            var result = controller.BasketSummary() as PartialViewResult;
+            var basketSummary = (BasketSummaryViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(3, basketSummary.BasketCount);
+            Assert.AreEqual(25, basketSummary.BasketTotal);
+
+        }
+
+        public BasketControllerTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+    }
+}
--- MyShop.Core/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyShop.Core/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -4,26 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using MyShop.WebUI;
 using MyShop.WebUI.Controllers;
+using MyShop.WebUI.Tests.Mocks;
 
 namespace MyShop.WebUI.Tests.Controllers
 {
     [TestClass]
     public class HomeControllerTest
     {
-        //[TestMethod]
-        //public void Index()
-        //{
-        //    // Arrange
-        //    HomeController controller = new HomeController();
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            IRepository<Product> productContext = new MockContext<Product>();
+            IRepository<ProductCategory> productCategoryContext = new MockContext<ProductCategory>();
 
-        //    // Act
-        //    ViewResult result = controller.Index() as ViewResult;
+            HomeController controller = new HomeController(productContext, productCategoryContext);
 
-        //    // Assert
-        //    Assert.IsNotNull(result);
-        //}
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
 
         //[TestMethod]
         //public void About()
